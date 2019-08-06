60068d3c74bc3812a45c9d15447864b15b673f7f
denlysenko <den.lysenko.mail@gmail.com>
2019-08-06 16:30:22 +0300

    revealing module

---
+++ revealing-module.js
@@ -0,0 +1,25 @@
+const myRevealingModule = (function() {
+  let privateVar = 'Ben Cherry',
+    publicVar = 'Hey there!';
+
+  function privateFunction() {
+    console.log('Name:' + privateVar);
+  }
+
+  function publicSetName(strName) {
+    privateVar = strName;
+  }
+
+  function publicGetName() {
+    privateFunction();
+  }
+
+  // Reveal public pointers to
+  // private functions and properties
+
+  return {
+    setName: publicSetName,
+    greeting: publicVar,
+    getName: publicGetName
+  };
+})();
