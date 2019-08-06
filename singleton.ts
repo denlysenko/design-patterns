b0d4fb97d21a87c21efa338fb0669b693c7f2d63
denlysenko <den.lysenko.mail@gmail.com>
2019-08-06 18:18:04 +0300

    singleton

---
+++ singleton.ts
@@ -0,0 +1,62 @@
+// TypeScript
+class Singleton {
+  private static instance: Singleton;
+
+  private constructor() {}
+
+  public static getInstance(): Singleton {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton();
+    }
+
+    return Singleton.instance;
+  }
+
+  public someBusinessLogic() {
+    // ...
+  }
+}
+
+// JavaScript
+const mySingleton = (function() {
+  // Instance stores a reference to the Singleton
+  let instance;
+
+  function init() {
+    // Singleton
+
+    // Private methods and variables
+    function privateMethod() {
+      console.log('I am private');
+    }
+
+    let privateVariable = 'Im also private';
+
+    let privateRandomNumber = Math.random();
+
+    return {
+      // Public methods and variables
+      publicMethod: function() {
+        console.log('The public can see me!');
+      },
+
+      publicProperty: 'I am also public',
+
+      getRandomNumber: function() {
+        return privateRandomNumber;
+      }
+    };
+  }
+
+  return {
+    // Get the Singleton instance if one exists
+    // or create one if it doesn't
+    getInstance: function() {
+      if (!instance) {
+        instance = init();
+      }
+
+      return instance;
+    }
+  };
+})();
